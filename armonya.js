abff45d2b9788a4abf1ea1c4787615af2197031f
Legend916 <75948369+Legend916@users.noreply.github.com>
2020-12-13 11:36:34 -0700

    First Commit

---
+++ armonya.js
@@ -0,0 +1,17 @@
+// let active = document.getElementsByClassName('active');
+let rows = document.getElementsByClassName('row');
+let rightButton = document.getElementById('rightButton');
+let x = 0;
+let y = 0;
+
+console.log(rows);
+console.log(rightButton);
+// console.log(active[0]);
+
+rightButton.onclick = function (e) {
+
+};
+
+function setActive(x, y) {
+
+}

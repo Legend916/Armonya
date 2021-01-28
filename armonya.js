b171c77a27457d947f0d4ab502f929ce9e6da1fd
Carson Taylor <carsontaylor1112@gmail.com>
2021-01-27 20:05:45 -0700

    added up/down/right/left

--- armonya.js
+++ armonya.js
@@ -4,17 +4,42 @@ let rightButton = document.getElementById('rightButton');
 let x = 0;
 let y = 0;
 
-console.log(rows);
-console.log(rightButton);
-// console.log(active[0]);
 
-rightButton.onclick = function (e) {
 
-};
+function logKey(e) {
+    console.log(` ${e.code}`);
+    const activeElement = document.getElementsByClassName('active')[0];
+    switch (e.code) {
+        case "ArrowLeft":
+            if (activeElement.previousElementSibling != null) {
+                activeElement.classList.remove("active");
+                activeElement.previousElementSibling.classList.add("active");
+                x--;
+            }
+            break;
+        case "ArrowRight":
+            if (activeElement.nextElementSibling != null) {
+                activeElement.classList.remove("active");
+                activeElement.nextElementSibling.classList.add("active");
+                x++;
+            }
+            break;
+        case "ArrowDown":
+            if (activeElement.parentElement.nextElementSibling != null) {
+                activeElement.classList.remove("active");
+                activeElement.parentElement.nextElementSibling.children[x].classList.add("active");
+            }
+            break;
+        case "ArrowUp":
+            if (activeElement.parentElement.previousElementSibling != null) {
+                activeElement.classList.remove("active");
+                activeElement.parentElement.previousElementSibling.children[x].classList.add("active");
+            }
 
-function setActive(x, y) {
+    }
+    console.log(x);
 }
 
-function setPElement {
-
-}
+document.addEventListener("DOMContentLoaded", function () {
+    document.addEventListener('keydown', logKey);
+});
